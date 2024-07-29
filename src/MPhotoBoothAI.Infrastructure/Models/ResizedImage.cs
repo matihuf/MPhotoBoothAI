64c7138d66c74f455b9010e1195a2d756aa4688a
matihuf <matihuf@hotmail.com>
2024-07-29 14:01:38 +0200

    Added ResizeImageService

---
+++ src/MPhotoBoothAI.Infrastructure/Models/ResizedImage.cs
@@ -0,0 +1,13 @@
+﻿
+using Emgu.CV;
+
+namespace MPhotoBoothAI.Infrastructure.Models;
+
+public class ResizedImage(Mat image, int newh, int neww, int top, int left)
+{
+    public Mat Image { get; private set; } = image;
+    public int Newh { get; private set; } = newh;
+    public int Neww { get; private set; } = neww;
+    public int Top { get; private set; } = top;
+    public int Left { get; private set; } = left;
+}
---
+++ tests/MPhotoBoothAI.Integration.Tests/Infrastructure/Services/ResizeImageServiceTests.cs
@@ -0,0 +1,44 @@
+﻿using Emgu.CV;
+using MPhotoBoothAI.Infrastructure;
+
+namespace MPhotoBoothAI.Integration.Tests;
+
+public class ResizeImageServiceTests
+{
+    private readonly ResizeImageService _resizeImageService;
+
+    public ResizeImageServiceTests()
+    {
+        _resizeImageService = new ResizeImageService();
+    }
+
+    [Fact]
+    public void Resize_KeepRatio_ShouldBeAsExpected()
+    {
+        //arrange
+        using var expected = CvInvoke.Imread("TestData/womanResizedKeepRatio.jpg");
+        using var frame = CvInvoke.Imread("TestData/woman.jpg");
+        //act
+        var result = _resizeImageService.Resize(frame, 640, 640, true);
+        //assert
+        string tmpFile = "resizeKeepRatio.jpg";
+        CvInvoke.Imwrite(tmpFile, result.Image);
+        using var x = CvInvoke.Imread(tmpFile);
+        Assert.True(expected.Equals(x));
+    }
+
+    [Fact]
+    public void Resize_DoNotKeepRatio_ShouldBeAsExpected()
+    {
+        //arrange
+        using var expected = CvInvoke.Imread("TestData/womanResizedDoNotKeepRatio.jpg");
+        using var frame = CvInvoke.Imread("TestData/woman.jpg");
+        //act
+        var result = _resizeImageService.Resize(frame, 640, 640, false);
+        //assert
+        string tmpFile = "resizeDoNotKeepRatio.jpg";
+        CvInvoke.Imwrite(tmpFile, result.Image);
+        using var x = CvInvoke.Imread(tmpFile);
+        Assert.True(expected.Equals(x));
+    }
+}
