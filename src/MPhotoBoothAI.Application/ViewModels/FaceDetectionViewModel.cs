3e5459aa1b734246a537353550ea24764dde233c
matihuf <matihuf@hotmail.com>
2024-08-05 10:26:58 +0000

    Dispose Frame while disposing viewmodel

--- src/MPhotoBoothAI.Application/ViewModels/FaceDetectionViewModel.cs
+++ src/MPhotoBoothAI.Application/ViewModels/FaceDetectionViewModel.cs
@@ -3,7 +3,7 @@ using Emgu.CV;
 
 namespace MPhotoBoothAI.Application.ViewModels;
 
-public partial class FaceDetectionViewModel : ViewModelBase, IObserver
+public partial class FaceDetectionViewModel : ViewModelBase, IObserver, IDisposable
 {
     private readonly IYoloFaceService _yoloFaceService;
     private readonly ICameraService _cameraService;
@@ -24,5 +24,20 @@ public partial class FaceDetectionViewModel : ViewModelBase, IObserver
         _yoloFaceService.Run(mat, 0.45f, 0.5f);
         Frame = mat.Clone();
         mat.Dispose();
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            _cameraService.Detach(this);
+            Frame.Dispose();
+        }
     }
 }
