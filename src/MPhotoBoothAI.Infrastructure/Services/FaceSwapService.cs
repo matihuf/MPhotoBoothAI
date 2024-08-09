c0b14f25956d98fb928a4fb54bb1108f13a8f196
matihuf <matihuf@hotmail.com>
2024-08-09 14:50:26 +0000

    Added face swap service

---
+++ src/MPhotoBoothAI.Infrastructure/Services/FaceSwapService.cs
@@ -0,0 +1,85 @@
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Dnn;
+using Emgu.CV.Util;
+
+namespace MPhotoBoothAI.Infrastructure;
+
+public class FaceSwapService(Net arfaceNet, Net gNet)
+{
+    private readonly Net _arfaceNet = arfaceNet;
+    private readonly Net _gNet = gNet;
+    private const float _normalizeFactor = 127.5f;
+
+    public Mat Swap(Mat sourceFace, Mat targetFace)
+    {
+        using var sourceFaceHalf = HalfSize(sourceFace);
+        using var sourceBlob = Preprocess(sourceFaceHalf);
+        _arfaceNet.SetInput(sourceBlob, "img");
+        using var arfaceNetOuts = new VectorOfMat();
+        _arfaceNet.Forward(arfaceNetOuts, _arfaceNet.UnconnectedOutLayersNames);
+
+        using var resizedTarget = new Mat();
+        CvInvoke.Resize(targetFace, resizedTarget, new Size(256, 256));
+        using var targetBlob = Preprocess(resizedTarget);
+        _gNet.SetInput(arfaceNetOuts[0], "source_emb");
+        _gNet.SetInput(targetBlob, "target");
+        using var gOuts = new VectorOfMat();
+        _gNet.Forward(gOuts, _gNet.UnconnectedOutLayersNames);
+        using var image = ConvertToImage(gOuts[0]);
+        var originalImageSize = new Mat();
+        CvInvoke.Resize(image, originalImageSize, sourceFace.Size);
+        return originalImageSize;
+    }
+
+    public static Mat ConvertToImage(Mat gOut)
+    {
+        var data = (float[,,,])gOut.GetData();
+        int channels = data.GetLength(1);
+        int height = data.GetLength(2);
+        int width = data.GetLength(3);
+
+        byte[,,] result = new byte[width, height, channels];
+        for (int h = 0; h < height; h++)
+        {
+            for (int w = 0; w < width; w++)
+            {
+                for (int c = 0; c < channels; c++)
+                {
+                    result[w, h, c] = (byte)Denormalize(data[0, c, w, h]);
+                }
+            }
+        }
+        GCHandle handle = GCHandle.Alloc(result, GCHandleType.Pinned);
+        var image = new Mat(256, 256, DepthType.Cv8U, 3, handle.AddrOfPinnedObject(), 0);
+        handle.Free();
+        return image;
+    }
+
+    private static float Denormalize(float normalized) => normalized * _normalizeFactor + _normalizeFactor;
+
+    private static Mat Preprocess(Mat face)
+    {
+        using var preprocesses = new Mat();
+        CvInvoke.CvtColor(face, preprocesses, ColorConversion.Bgra2Rgb);
+        using var normalized = Normalize(preprocesses);
+        return DnnInvoke.BlobFromImage(normalized, 1, face.Size);
+    }
+
+    private static Mat HalfSize(Mat face)
+    {
+        var halfSize = new Mat();
+        var size = new Size(face.Width / 2, face.Height / 2);
+        CvInvoke.Resize(face, halfSize, size);
+        return halfSize;
+    }
+
+    private static Mat Normalize(Mat face)
+    {
+        var normalizedImg = new Mat();
+        face.ConvertTo(normalizedImg, DepthType.Cv32F, 1.0 / _normalizeFactor, -1.0);
+        return normalizedImg;
+    }
+}
---
+++ tests/MPhotoBoothAI.Integration.Tests/Infrastructure/Services/FaceSwapServiceTests.cs
@@ -0,0 +1,38 @@
+﻿using Emgu.CV;
+using Emgu.CV.Dnn;
+using MPhotoBoothAI.Infrastructure;
+using MPhotoBoothAI.Infrastructure.Services;
+
+namespace MPhotoBoothAI.Integration.Tests.Infrastructure.Services;
+
+public class FaceSwapServiceTests
+{
+    [Fact]
+    public void Swap_ShouldReturnExpected()
+    {
+        //arrange
+        using var expected = CvInvoke.Imread("TestData/womanSwapped.jpg");
+        using var sourceFaceFrame = CvInvoke.Imread("TestData/woman.jpg");
+        using var targetFaceFrame = CvInvoke.Imread("TestData/woman2.jpg");
+        using var yoloNet = DnnInvoke.ReadNetFromONNX("yolov8n-face.onnx");
+        var faceAlignService = new FaceAlignService();
+        var faceDetectionService = new FaceDetectionService(yoloNet, new ResizeImageService());
+
+        using var sourceFace = faceDetectionService.Detect(sourceFaceFrame, 0.8f, 0.5f).First();
+        using var sourceAlignFace = faceAlignService.Align(sourceFace.Frame, sourceFace.Landmarks);
+
+        using var targetFace = faceDetectionService.Detect(targetFaceFrame, 0.8f, 0.5f).First();
+        using var targetAlignFace = faceAlignService.Align(targetFace.Frame, targetFace.Landmarks);
+
+        using var arcfaceNet = DnnInvoke.ReadNetFromONNX("arcface_backbone.onnx");
+        using var gNet = DnnInvoke.ReadNetFromONNX("G_unet_2blocks.onnx");
+        var faceSwapService = new FaceSwapService(arcfaceNet, gNet);
+        //act
+        using var result = faceSwapService.Swap(sourceAlignFace, targetAlignFace);
+        //assert
+        string tmpFile = "womanSwappedTmp.jpg";
+        CvInvoke.Imwrite(tmpFile, result);
+        using var x = CvInvoke.Imread(tmpFile);
+        Assert.True(expected.Equals(x));
+    }
+}
