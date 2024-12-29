cf0e5ea9c968ab2d050312afd2cbe85b2bc4b24a
MrRadian <62394971+MrRadian@users.noreply.github.com>
2024-12-29 17:35:12 +0100

    Switch to dictionary typer viewModel

---
+++ src/MPhotoBoothAI.Application/Models/BackgroundInfo.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace MPhotoBoothAI.Application.Models;
+public partial class BackgroundInfo : ObservableObject
+{
+    [ObservableProperty]
+    private string? _backgroundPath;
+
+    [ObservableProperty]
+    private string? _selectedItem;
+
+    [ObservableProperty]
+    private ObservableCollection<string> _backgroundPathsList = [];
+}
