9f069d629f366452a981bff1bb990a9c5a5fa373
MrRadian <62394971+MrRadian@users.noreply.github.com>
2025-01-09 19:17:39 +0100

    Create correct database migration

---
+++ src/MPhotoBoothAI.Models/Models/OverlayImageData.cs
@@ -0,0 +1,11 @@
+﻿namespace MPhotoBoothAI.Models
+{
+    public class OverlayImageData
+    {
+        public string Path { get; set; }
+        public double Top { get; set; }
+        public double Left { get; set; }
+        public double Angle { get; set; }
+        public double Scale { get; set; }
+    }
+}
---
+++ src/MPhotoBoothAI.Models/Models/PhotoLayoutData.cs
@@ -0,0 +1,10 @@
+﻿namespace MPhotoBoothAI.Models
+{
+    public class PhotoLayoutData
+    {
+        public double Top { get; set; }
+        public double Left { get; set; }
+        public double Angle { get; set; }
+        public double Scale { get; set; }
+    }
+}
